0b8f21c941b1b33cc33ae43c2299a58d229dab7f
Huw Diprose <mail@huwdiprose.co.uk>
2015-11-15 11:25:27 +0000

    SEO - minifies form and init.js

--- js/form.js
+++ js/form.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-    // Hide additional participants based on selection
-    $('.hidden_div').hide();
-
-    $('#entry_87174897').change(function() {
-	  $('.hidden_div').hide();
-	  $('.' + this.value).fadeIn();
-    });
-
-	$("#ss-form").validate({meta: "validate"});
-});
+$(document).ready(function(){$(".hidden_div").hide();$("#entry_87174897").change(function(){$(".hidden_div").hide();$("."+this.value).fadeIn()});$("#ss-form").validate({meta:"validate"})});
